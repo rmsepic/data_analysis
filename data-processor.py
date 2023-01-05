3ae6c92339834a0d3d7d1f2e1028b855c04c2e3b
rmsepic <rmsepic@ucdavis.edu>
2023-01-04 23:03:02 -0800

    Import numbers file

--- data-processor.py
+++ data-processor.py
@@ -1,7 +1,15 @@
 import pandas as pd 
 
-df = pd.read_csv(r'job-hunt.csv')
+from numbers_parser import Document
 
-currently_pending_jobs = df['Status'].value_counts()
-print(currently_pending_jobs)
+doc = Document("jobs.numbers")
+sheets = doc.sheets
+tables = sheets[0].tables
+data = tables[0].rows(values_only=True)
+df = pd.DataFrame(data[1:], columns=data[0])
+
+print(df)
+
+#currently_pending_jobs = df['Status'].value_counts()
+#print(currently_pending_jobs)
 
